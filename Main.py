b8dc3d55509175c2e2730f8dd8bc1689d05fc1e6
Sonya <sofico86solo@yandex.ru>
2024-11-01 14:44:26 +0300

    Добавлены слова широта и долгота

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def get_coord(city,key):
         if results:
             lat= round(results[0]['geometry']['lat'],2)
             lng= round(results[0]['geometry']['lng'],2)
-            return lat,lng
+            return f"широта: {lat}, долгота: {lng}"
         else:
             return "Город не найден!"
     except Exception as e:
