ed8c925b5ed3599c385e78a526881ee2bb5517c8
Sonya <sofico86solo@yandex.ru>
2024-11-02 16:33:47 +0300

    Добавлена валюта города и кнопка для очистки полей, а также окно с ошибкой, если город не введен или не найден

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
 import webbrowser
+from tkinter import messagebox as mb
 
 def get_coord(city,key):
     try:
@@ -10,19 +11,20 @@ def get_coord(city,key):
             lat= round(results[0]['geometry']['lat'],2)
             lon= round(results[0]['geometry']['lng'],2)
             country=results[0]['components']['country']
+            valuta=results[0]['annotations']['currency']['name']
+            #print(valuta)
             osm_url=f"https://www.openstreetmap.org/?mlat={lat}&mlon={lon}"
             if 'state' in results[0]['components']:
                 region=results[0]['components']['state']
                 return {
-                    "coord":f"широта: {lat}, долгота: {lon} \n страна: {country} регион: {region}",
-                    "map_url":osm_url }
+                    "coord":f"широта: {lat}, долгота: {lon} ,\nстрана: {country}, регион: {region},\n валюта :{valuta}","map_url":osm_url
+                    }
             else:
                 return {
                     "coord": f"широта: {lat}, долгота: {lon} \n страна: {country}",
-                    "map_url": osm_url}
-
+                    "map_url": osm_url,"валюта":{valuta}}
         else:
-            return "Город не найден!"
+            mb.showerror("Ошибка","Город не найден!")
     except Exception as e:
         return f"Возникла ошибка: {e}"
 
@@ -34,16 +36,22 @@ def show_coord(event=None):
     map_url=rezult['map_url']
 
 def show_map():
+    global map_url
     if map_url:
         webbrowser.open(map_url)
 
+def clear_all():
+    global map_url
+    label.config(text="")
+    entry.delete(0,END)
+    map_url=None
 
 key='420a69538d734305b55566a2e5894b65'
 map_url=""
 
 win=Tk()
 win.title("Координаты городов")
-win.geometry("400x150")
+win.geometry("450x250")
 
 entry=Entry()
 entry.pack()
@@ -55,5 +63,6 @@ label=Label(text="Введите город и нажмите на кнопку 
 label.pack()
 
 map_button=Button(text="Показать",command=show_map).pack()
+clear_button=Button(text="Очистить",command=clear_all).pack()
 
 win.mainloop()
